43597054c1b76544e9796b059f08b90e936ec540
Renato Carneiro <renato.carneirofs@gmail.com>
2021-07-06 16:52:43 -0300

    commit2

--- api/backend.js
+++ api/backend.js
@@ -7,69 +7,105 @@ app.use(express.json())
 
 let axios = require("axios").default;
 
-let optionsHero = {
-  method: 'GET',
-  url: 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/types/hero',
-  headers: {
-    'x-rapidapi-key': '5dbee0d520mshda247591d161b4ap12c278jsn6924d09fa87a',
-    'x-rapidapi-host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
+let types = (type) => {
+  return {
+    method: 'GET',
+    url: 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/types/' + type,
+    headers: {
+      'x-rapidapi-key': '5dbee0d520mshda247591d161b4ap12c278jsn6924d09fa87a',
+      'x-rapidapi-host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
+    }
   }
 };
 
-app.get('/typesHero', function(req, res){
-    axios.request(optionsHero).then(function (response) {
-        let resp = response.data;
-        resp = resp.splice(0,29);
-        console.log(resp.length)
-        res.json(resp)
-        
-    }).catch(function (error) {
-        console.error(error);        
-    });
+app.get('/types/:type', function (req, res) {
+  const { type } = req.params
+  axios.request(types(type)).then(function (response) {
+    let resp = response.data;
+    resp = resp.splice(0, 29);
+    console.log(resp.length)
+    res.json(resp)
+
+  }).catch(function (error) {
+    console.error(error);
+  });
 });
 
-let optionsMinion = {
-  method: 'GET',
-  url: 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/types/minion',
-  headers: {
-    'x-rapidapi-key': '5dbee0d520mshda247591d161b4ap12c278jsn6924d09fa87a',
-    'x-rapidapi-host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
+
+let factions = (faction) => {
+  return {
+    method: 'GET',
+    url: 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/factions/' + faction,
+    headers: {
+      'x-rapidapi-key': '5dbee0d520mshda247591d161b4ap12c278jsn6924d09fa87a',
+      'x-rapidapi-host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
+    }
   }
 };
 
-app.get('/typesMinion', function(req, res){
-    axios.request(optionsMinion).then(function (response) {
-        const resp = response.data;
-        res.json(resp)
-        
-    }).catch(function (error) {
-        console.error(error);        
-    });
+app.get('/factions/:faction', function (req, res) {
+  const { faction } = req.params
+    axios.request(factions(faction)).then(function (response) {
+    let resp = response.data;
+    resp = resp.splice(0, 29);
+    console.log(resp.length)
+    res.json(resp)
+
+  }).catch(function (error) {
+    console.error(error);
+  });
 });
 
-let optionsSpell = {
-  method: 'GET',
-  url: 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/types/spell',
-  headers: {
-    'x-rapidapi-key': '5dbee0d520mshda247591d161b4ap12c278jsn6924d09fa87a',
-    'x-rapidapi-host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
+let races = (race) => {
+  return {
+    method: 'GET',
+    url: 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/races/' + race,
+    headers: {
+      'x-rapidapi-key': '5dbee0d520mshda247591d161b4ap12c278jsn6924d09fa87a',
+      'x-rapidapi-host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
+    }
   }
 };
 
-app.get('/typesSpell', function(req, res){
-    axios.request(optionsSpell).then(function (response) {
-        const resp = response.data;
-        res.json(resp)
-        
-    }).catch(function (error) {
-        console.error(error);        
-    });
+app.get('/races/:race', function (req, res) {
+  const { race } = req.params
+    axios.request(races(race)).then(function (response) {
+    let resp = response.data;
+    resp = resp.splice(0, 15);
+    console.log(resp.length)
+    res.json(resp)
+
+  }).catch(function (error) {
+    console.error(error);
+  });
 });
 
+let qualities = (qualitie) => {
+  return {
+    method: 'GET',
+    url: 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/qualities/' + qualitie,
+    headers: {
+      'x-rapidapi-key': '5dbee0d520mshda247591d161b4ap12c278jsn6924d09fa87a',
+      'x-rapidapi-host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
+    }
+  }
+};
 
+app.get('/qualities/:qualitie', function (req, res) {
+  const {qualitie } = req.params
+    axios.request(qualities(qualitie)).then(function (response) {
+    let resp = response.data;
+    resp = resp.splice(0, 29);
+    console.log(resp.length)
+    res.json(resp)
 
-app.listen(3000, function(){
-    console.log('Server is running! Run Bitch, Run')
+  }).catch(function (error) {
+    console.error(error);
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Server is running! Run Bitch, Run')
 });
 
 
