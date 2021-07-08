35a0b95aad280d1a2ddb247081d88a0bd29f5553
Renato Carneiro <renato.carneirofs@gmail.com>
2021-07-08 14:40:51 -0300

    Update reload cartas
    Reload de cartas por filtro

--- app/index.js
+++ app/index.js
@@ -1,10 +1,32 @@
 const hero = document.querySelector("#hero");
 const item = document.querySelector("#qtd");
+let lastSelect = '';
+let lastValue = '';
+let pageSize = 15;
+
+function lastTypes(last, value){
+	lastSelect = last
+	lastValue = value
+}
+
+function reload(){
+	if (lastSelect){
+		types(lastSelect, lastValue)
+	}
+}
+
+function setPageSize(num){
+	pageSize = num
+}
+
+item.addEventListener('change', function() {     
+	setPageSize(this.value)     
+	reload() 
+})
 
 const types = async (opt, opt2) => {
+	lastTypes(opt, opt2)
     document.getElementById("section").innerHTML = ''
-	const pageSize = item.value
-	console.log(pageSize)
     const listaFactions = await fetch(`http://localhost:3000/${opt}/${opt2}?pageSize=${pageSize}`)
     const respostaFactions = await listaFactions.json()
 	respostaFactions.map((itemFactions) => {
